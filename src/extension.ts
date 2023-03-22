54f122fae683b133aa32bc4f711e97dea5140790
Zoey Zuo <zoeyzuouk@gmail.com>
2023-03-22 16:16:54 +0000

    add helper functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {combiner} from './helper';
+const fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,7 +22,23 @@ export function activate(context: vscode.ExtensionContext) {
         if (uris && uris.length === 2) {
             // Combine the policies
 			vscode.window.showInformationMessage(`2 files selected: ${uris[0]}, ${uris[1]}`);
-            // ...
+			var csPath = '';
+			var xmlPath = '';
+			if(uris[0].path.endsWith('.cs') && uris[1].path.endsWith('.xml')){
+				csPath = uris[0].path;
+				xmlPath = uris[1].path;
+			} else if(uris[0].path.endsWith('.xml') && uris[1].path.endsWith('.cs')){
+				csPath = uris[1].path;
+				xmlPath = uris[0].path;
+			} else {
+				vscode.window.showInformationMessage('Please select one c# file and one xml file');
+			}
+			const combinedXmlString = await combiner(xmlPath, csPath);
+			const outputFileLoc = "/Users/zoeyzuo/Documents/work/LSEG/extension-dev/azure-apim-policy-combiner/result.xml";
+			fs.writeFileSync(outputFileLoc, combinedXmlString);
+            const doc = await vscode.workspace.openTextDocument(outputFileLoc);
+            await vscode.window.showTextDocument(doc, { preview: false });
+
         } else {
             vscode.window.showInformationMessage('Please select two files to combine.');
         }
---
+++ src/helper.ts
@@ -0,0 +1,170 @@
+import { Uri } from "vscode";
+
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+
+export async function combiner(xmlPath: string, csPath: string): Promise<string> {
+    const xmlObject = await generateXmlObj(xmlPath);
+    const locators: string[]= await generateLocators(xmlObject);
+    const paths = locators.map((loc) => {
+        const value = getValueByLocator(xmlObject, loc);
+        if(isCsharpPath(value)) {
+            const fileName = csPath;
+            const methodName = getMethodName(value);
+            const code = `@{` + getCode(fileName, methodName) + `}`;
+            updateValueByLocator(xmlObject,loc,code);
+        }
+        return value;
+      });
+
+    // Convert the XML object back to an XML string
+    const builder = new xml2js.Builder();
+    const combinedXmlString = builder.buildObject(xmlObject);
+
+    return combinedXmlString;
+
+    // Write the combined XML to a file
+    // fs.writeFileSync('./result.xml', combinedXmlString);
+}
+
+function getCode(fileName: string, methodName: string ) {
+    const csharpCode = fs.readFileSync(require.resolve(fileName), 'utf-8');
+    // Extract the method code
+    const methodRegex = new RegExp(`(?<=\\b${methodName}\\b\\s*\\([^)]*\\)\\s*\\{)[^{}]*(?:\\{[^{}]*\\}[^{}]*)*(?=})`);
+    const methodMatch = csharpCode.match(methodRegex);
+    console.log(methodMatch[0]);
+
+    if (!methodMatch) {
+        console.error(`Method "${methodName}" not found in file "${fileName}"`);
+        process.exit(1);
+      }
+    return methodMatch ? methodMatch[0] : null;
+}
+
+interface XmlNode {
+    [key: string]: any;
+}
+  
+interface StackItem {
+    node: XmlNode;
+    locator: string;
+}
+
+export async function generateLocators(xmlObject: any): Promise<string[]> {
+
+      const locators: string[] = [];
+      const stack: StackItem[] = [{ node: xmlObject, locator: '' }];
+    
+      while (stack.length > 0) {
+        const { node, locator } = stack.pop()!;
+    
+        for (const key in node) {
+          const value = node[key];
+          const childLocator = `${locator}.${key}`;
+          if (key === 'value') {
+            locators.push(transformLocatorString(childLocator));
+          } else if (Array.isArray(value)) {
+            for (let i = 0; i < value.length; i++) {
+              stack.push({ node: value[i], locator: `${childLocator}[${i}]` });
+            }
+          } else if (typeof value === 'object') {
+            stack.push({ node: value, locator: childLocator });
+          }
+        }
+      }
+    
+      return locators;
+  }
+
+// tested
+function getValueByLocator(xmlObject: XmlNode, locator: string): any {
+    if (!xmlObject) {
+      return undefined;
+    }
+  
+    const keys = locator.split('.').map((key) => {
+      if (key.endsWith(']')) {
+        const [name, index] = key.slice(0, -1).split('[');
+        return { name, index: parseInt(index) };
+      } else {
+        return { name: key };
+      }
+    });
+  
+    return keys.reduce((node: any, { name, index }) => {
+      if (!node || node[name] === undefined) {
+        return undefined;
+      }
+  
+      if (index !== undefined) {
+        return node[name][index];
+      } else {
+        return node[name];
+      }
+    }, xmlObject);
+}
+
+// tested
+function updateValueByLocator(xmlObject: XmlNode, locator: string, newValue: any): void {
+    const keys = locator.split('.').map((key) => {
+      if (key.endsWith(']')) {
+        const [name, index] = key.slice(0, -1).split('[');
+        return { name, index: parseInt(index) };
+      } else {
+        return { name: key };
+      }
+    });
+  
+    const finalKey = keys.pop();
+    if (!finalKey || !finalKey.name) {
+      return;
+    }
+  
+    const node = keys.reduce((node: any, { name, index }) => {
+      if (!node || node[name] === undefined) {
+        return undefined;
+      }
+  
+      if (index !== undefined) {
+        return node[name][index];
+      } else {
+        return node[name];
+      }
+    }, xmlObject);
+  
+    if (node && node[finalKey.name] !== undefined) {
+      if (finalKey.index !== undefined) {
+        node[finalKey.name][finalKey.index] = newValue;
+      } else {
+        node[finalKey.name] = newValue;
+      }
+    }
+}
+
+function isCsharpPath(value: string): boolean {
+    return value.startsWith('@{') && value.endsWith('}');
+}
+
+// function getFilePath(value: string): string {
+//     var fileNameMethod = value.replace('@{','');
+//     fileNameMethod = fileNameMethod.replace('}', '');
+//     return fileNameMethod.split(' ')[0];
+// }
+
+function getMethodName(value: string): string {
+    var fileNameMethod = value.replace('@{','');
+    fileNameMethod = fileNameMethod.replace('}', '');
+    return fileNameMethod.split(' ')[1];
+}
+export async function generateXmlObj(absoluteFilePath: string) {
+    const xmlString = fs.readFileSync(require.resolve(absoluteFilePath), 'utf-8');
+    return await xml2js.parseStringPromise(xmlString);
+}
+
+function transformLocatorString(locator: string): string {
+    if (locator.startsWith('.')) {
+        locator = locator.substring(1);
+    }
+    return `${locator}[0]`;
+}
