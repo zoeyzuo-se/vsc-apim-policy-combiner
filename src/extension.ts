a4dcccbdabeff56856d32dff37e903a98db11f27
Zoey Zuo <zoeyzuouk@gmail.com>
2023-04-05 12:27:09 +0100

    Use local NPM package

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import {combiner} from './helper';
+const transformer = require('apim-policy-transformer');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -18,44 +18,18 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('azure-apim-policy-combiner.policyCombiner', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		const uris = await vscode.window.showOpenDialog({ canSelectMany: true });
-		var resultLocation = <any>vscode.workspace.workspaceFolders?.[0].uri.fsPath || undefined;
-        if (uris && uris.length === 2) {
+		const uri = await vscode.window.showOpenDialog({
+			canSelectMany: false,
+			canSelectFiles: false,
+			canSelectFolders: true,
+		});
+		let policyDir = uri?.[0].fsPath;
+        if (policyDir) {
             // Combine the policies
-			vscode.window.showInformationMessage(`2 files selected: ${uris[0]}, ${uris[1]}`);
-			var csPath = '';
-			var xmlPath = '';
-			if(uris[0].fsPath.endsWith('.cs') && uris[1].fsPath.endsWith('.xml')){
-				csPath = uris[0].fsPath;
-				xmlPath = uris[1].fsPath;
-			} else if(uris[0].fsPath.endsWith('.xml') && uris[1].fsPath.endsWith('.cs')){
-				csPath = uris[1].fsPath;
-				xmlPath = uris[0].fsPath;
-			} else {
-				vscode.window.showInformationMessage('Please select one c# file and one xml file');
-			}
-			const combinedXmlString = await combiner(xmlPath, csPath);
-			if (!resultLocation) {
-				vscode.window.showInformationMessage('Please choose a location to store the combined file');
-				const resultLocationUri = await vscode.window.showOpenDialog( {
-					openLabel: 'Select location',
-					canSelectFiles: false,
-					canSelectFolders: true,
-					canSelectMany: false,
-				});
-				resultLocation = resultLocationUri?.[0].fsPath;
-			}
-			if(!resultLocation) {
-				vscode.window.showErrorMessage('Please select a location to store the result file');
-			} else {
-				let dateTime = new Date();
-				const outputFileLoc = `${resultLocation}/result-${dateTime.toUTCString()}.xml`;
-				fs.writeFileSync(outputFileLoc, combinedXmlString);
-				const doc = await vscode.workspace.openTextDocument(outputFileLoc);
-				await vscode.window.showTextDocument(doc, { preview: false });
-			}     
+			vscode.window.showInformationMessage(`1 location selected: ${policyDir}`);
+			transformer.extractor(policyDir);
         } else {
-            vscode.window.showInformationMessage('Please select two files to combine.');
+            vscode.window.showInformationMessage('Please select only one directory');
         }
 	});
 
