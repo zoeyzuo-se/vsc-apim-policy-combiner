bb46af9776f2357e9c70d0fbd02ec82d53424869
Zoey Zuo <zoeyzuouk@gmail.com>
2023-04-14 19:35:52 +0100

    Config lint indention rules and apply

--- src/extension.ts
+++ src/extension.ts
@@ -1,78 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 const path = require('path');
 const transformer = require('apim-policy-transformer');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsc-apim-policy-transformer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let policyCombiner = vscode.commands.registerCommand('vsc-apim-policy-transformer.policyCombiner', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const uri = await vscode.window.showOpenDialog({
-			canSelectMany: false,
-			canSelectFiles: false,
-			canSelectFolders: true,
-		});
-		let dir = uri?.[0].fsPath;
+    let policyCombiner = vscode.commands.registerCommand('vsc-apim-policy-transformer.policyCombiner', async () => {
+        const uri = await vscode.window.showOpenDialog({
+            canSelectMany: false,
+            canSelectFiles: false,
+            canSelectFolders: true,
+        });
+        let dir = uri?.[0].fsPath;
         if (dir) {
             // Combine the policies
-			transformer.combiner(dir);
-			vscode.window.showInformationMessage(`Combine finished`);
+            transformer.combiner(dir);
+            vscode.window.showInformationMessage(`Combine finished`);
         } else {
             vscode.window.showInformationMessage('Please select only one directory');
         }
-	});
+    });
 
-	let policyExtractor = vscode.commands.registerCommand('vsc-apim-policy-transformer.policyExtractor', async () => {
-		const uri = await vscode.window.showOpenDialog({
-			canSelectMany: false,
-			canSelectFiles: false,
-			canSelectFolders: true,
-		});
-		let dir = uri?.[0].fsPath;
+    let policyExtractor = vscode.commands.registerCommand('vsc-apim-policy-transformer.policyExtractor', async () => {
+        const uri = await vscode.window.showOpenDialog({
+            canSelectMany: false,
+            canSelectFiles: false,
+            canSelectFolders: true,
+        });
+        let dir = uri?.[0].fsPath;
         if (dir) {
-            // Extract the policies
-			transformer.extractor(dir);
-			vscode.window.showInformationMessage(`Extract finished`);
+            transformer.extractor(dir);
+            vscode.window.showInformationMessage(`Extract finished`);
         } else {
             vscode.window.showInformationMessage('Please select only one directory');
         }
-	});
+    });
 
-	let combinePolicyFromDirectory = vscode.commands.registerCommand('vsc-apim-policy-transformer.combineFromDirectory', async (uri) => {
-		if (uri && uri.scheme === 'file' && uri.fsPath && !path.basename(uri.fsPath).includes('.')) {
-			transformer.combiner(uri.fsPath);
-			vscode.window.showInformationMessage(`Combine finished`);
+    let combinePolicyFromDirectory = vscode.commands.registerCommand('vsc-apim-policy-transformer.combineFromDirectory', async (uri) => {
+        if (uri && uri.scheme === 'file' && uri.fsPath && !path.basename(uri.fsPath).includes('.')) {
+            transformer.combiner(uri.fsPath);
+            vscode.window.showInformationMessage(`Combine finished`);
 		  }
 		  else {
             vscode.window.showInformationMessage('Having trouble locating the directory');
         }
-	});
+    });
 
-	let extractPolicyFromDirectory = vscode.commands.registerCommand('vsc-apim-policy-transformer.extractFromDirectory', async (uri) => {
-		if (uri && uri.scheme === 'file' && uri.fsPath && !path.basename(uri.fsPath).includes('.')) {
-			transformer.extractor(uri.fsPath);
-			vscode.window.showInformationMessage(`Extract finished`);
+    let extractPolicyFromDirectory = vscode.commands.registerCommand('vsc-apim-policy-transformer.extractFromDirectory', async (uri) => {
+        if (uri && uri.scheme === 'file' && uri.fsPath && !path.basename(uri.fsPath).includes('.')) {
+            transformer.extractor(uri.fsPath);
+            vscode.window.showInformationMessage(`Extract finished`);
 		  } else {
-			vscode.window.showInformationMessage('Having trouble locating the directory');
+            vscode.window.showInformationMessage('Having trouble locating the directory');
 		  }
-	});
+    });
 
-	context.subscriptions.push(policyCombiner);
-	context.subscriptions.push(policyExtractor);
-	context.subscriptions.push(combinePolicyFromDirectory);
-	context.subscriptions.push(extractPolicyFromDirectory);
+    context.subscriptions.push(policyCombiner);
+    context.subscriptions.push(policyExtractor);
+    context.subscriptions.push(combinePolicyFromDirectory);
+    context.subscriptions.push(extractPolicyFromDirectory);
 }
 
 // This method is called when your extension is deactivated
